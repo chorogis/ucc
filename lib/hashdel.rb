84bbb24152dc72af4cb24fa3d6163027b401a6e4
chorogis <chorogis@gmail.com>
2015-02-06 11:56:59 +0900

    first

--- lib/hashdel.rb
+++ lib/hashdel.rb
@@ -1,16 +1,11 @@
+require 'json'
+
 class Hash
-  
+
   def bye(path)
-    ret = self
-    path.split('.').each do |p|
-      if p.to_i.to_s == p
-        ret = ret[p.to_i]
-      else
-        ret = ret[p.to_s] || ret[p.to_sym]
-      end
-      break unless ret
-    end
-    ret
+   kk=path.split("/")
+   for kkk in kk[0..-2] do jj=self[kkk] rescue nil end
+   jj.delete(kk[-1]) rescue nil
   end
 
   alias :hashdel :bye
